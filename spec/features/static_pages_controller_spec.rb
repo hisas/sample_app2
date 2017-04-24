1220afe49fbba9df964997ca7371e2da7c2f6e5d
Hisashi Kamezawa <hkame6926@gmail.com>
2017-04-24 15:15:09 +0900

    Add contact page and set links

--- spec/features/static_pages_controller_spec.rb
+++ spec/features/static_pages_controller_spec.rb
@@ -2,20 +2,26 @@ require 'rails_helper'
 
 describe "test static_pages_controller", type: :feature do
   it "should get home" do
-    visit '/static_pages/home'
+    visit root_path
     expect(page).to have_http_status(:success)
     expect(page).to have_selector 'title', exact_text: "Ruby on Rails Tutorial Sample App", visible: false
   end
 
   it "should get help" do
-    visit '/static_pages/help'
+    visit help_path
     expect(page).to have_http_status(:success)
     expect(page).to have_selector 'title', exact_text: "Help | Ruby on Rails Tutorial Sample App", visible: false
   end
 
   it "should get about" do
-    visit '/static_pages/about'
+    visit about_path
     expect(page).to have_http_status(:success)
     expect(page).to have_selector 'title', exact_text: "About | Ruby on Rails Tutorial Sample App", visible: false
+  end
+
+  it "should get contact" do
+    visit contact_path
+    expect(page).to have_http_status(:success)
+    expect(page).to have_selector 'title', exact_text: "Contact | Ruby on Rails Tutorial Sample App", visible: false
   end
 end
