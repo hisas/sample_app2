641a5251333dc80f6448afe912871f4044fa4df8
Hisashi Kamezawa <hkame6926@gmail.com>
2017-04-24 10:20:15 +0900

    Refactoring

--- spec/features/static_pages_controller_spec.rb
+++ spec/features/static_pages_controller_spec.rb
@@ -4,18 +4,18 @@ describe "test static_pages_controller", type: :feature do
   it "should get home" do
     visit '/static_pages/home'
     expect(page).to have_http_status(:success)
-    expect(page).to have_title('Home | Ruby on Rails Tutorial Sample App')
+    expect(page).to have_selector 'title', exact_text: "Ruby on Rails Tutorial Sample App", visible: false
   end
 
   it "should get help" do
     visit '/static_pages/help'
     expect(page).to have_http_status(:success)
-    expect(page).to have_title('Help | Ruby on Rails Tutorial Sample App')
+    expect(page).to have_selector 'title', exact_text: "Help | Ruby on Rails Tutorial Sample App", visible: false
   end
 
   it "should get about" do
     visit '/static_pages/about'
     expect(page).to have_http_status(:success)
-    expect(page).to have_title('About | Ruby on Rails Tutorial Sample App')
+    expect(page).to have_selector 'title', exact_text: "About | Ruby on Rails Tutorial Sample App", visible: false
   end
 end
