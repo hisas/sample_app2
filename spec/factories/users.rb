bbec7034d557ef32e63feee05b8d35552389f040
Hisashi Kamezawa <hkame6926@gmail.com>
2017-04-26 19:02:56 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,19 @@
 FactoryGirl.define do
   factory :user do
-    name "Michael Example"
-    email "michael@example.com"
+    sequence(:name) { |n| "User #{n}" }
+    sequence(:email) { |n| "user-#{n}@example.com" }
     password "password"
     password_digest User.digest("password")
+
+    factory :michael do
+      name "Michael Example"
+      email "michael@example.com"
+      admin true
+    end
+
+    factory :archer do
+      name "Sterling Archer"
+      email "duchess@example.gov"
+    end
   end
 end
