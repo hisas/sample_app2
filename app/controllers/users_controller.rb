5e36db0c25a8305a74f2736e02c31ec7eada25a7
Hisashi Kamezawa <hkame6926@gmail.com>
2017-04-27 10:50:26 +0900

    add action_args

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  permits :name, :email, :password, :password_confirmation
+
   def index
     @users = User.page params[:page]
   end
@@ -14,8 +16,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
+  def create(user)
+    @user = User.new(user)
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -29,9 +31,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def update
+  def update(user)
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    if @user.update_attributes(user)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -46,12 +48,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
     def logged_in_user
       unless logged_in?
         store_location
