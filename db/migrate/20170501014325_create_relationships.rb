2cd15c6e229279a8bdd14da998ef7dff55da2246
Hisashi Kamezawa <hkame6926@gmail.com>
2017-05-09 11:29:29 +0900

    Fix migration files
    null:false制約を付与した。
    followerとfollowedをreferencesに書き換えた。

--- db/migrate/20170501014325_create_relationships.rb
+++ db/migrate/20170501014325_create_relationships.rb
@@ -1,13 +1,11 @@
 class CreateRelationships < ActiveRecord::Migration[5.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.references :follower, index: true
+      t.references :followed, index: true
 
       t.timestamps
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
     add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
