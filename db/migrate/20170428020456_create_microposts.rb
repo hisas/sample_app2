7ba0b900eb99ad05676a0496d513fbe63378d575
Hisashi Kamezawa <hkame6926@gmail.com>
2017-05-09 11:29:29 +0900

    Fix migration files
    null:false制約を付与した。
    followerとfollowedをreferencesに書き換えた。

--- db/migrate/20170428020456_create_microposts.rb
+++ db/migrate/20170428020456_create_microposts.rb
@@ -1,7 +1,7 @@
 class CreateMicroposts < ActiveRecord::Migration[5.1]
   def change
     create_table :microposts do |t|
-      t.text :content
+      t.text :content, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170501014325) do
 
   create_table "microposts", force: :cascade do |t|
-    t.text "content"
+    t.text "content", null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 20170501014325) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
